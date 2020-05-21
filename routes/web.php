42b378907ea04ece03261fceb8cbdaabaf1e7185
Daryn-kun <daryn.123434@gmail.com>
2020-05-22 01:36:07 +0600

    updated

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,8 +12,60 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', 'MainController@index')->name('index');
-Route::get('/categories', 'MainController@categories')->name('categories');
-Route::get('/{category}', 'MainController@category')->name('category');
+Auth::routes([
+    'reset' => false,
+    'confirm' => false,
+    'verify' => false,
+]);
 
-Route::get('/mobiles/{product?}', 'MainController@product')->name('product');
+Route::get('locale/{locale}', 'MainController@changeLocale')->name('locale');
+
+Route::get('/logout','Auth\LoginController@logout')->name('get-logout');
+
+Route::middleware(['set_locale'])->group(function (){
+    Route::middleware(['auth'])->group(function (){
+        Route::group([
+            'prefix' => 'person',
+            'namespace' => 'Person',
+            'as' => 'person.',
+        ], function (){
+            Route::get('/orders', 'OrderController@index')->name('orders.index');
+            Route::get('/orders/{order}', 'OrderController@show')->name('orders.show');
+        });
+        Route::group([
+            'namespace' => 'Admin',
+            'prefix' => 'admin',
+        ], function (){
+            Route::group(['middleware' => 'is_admin'], function (){
+                Route::get('/orders', 'OrderController@index')->name('orders');
+                Route::get('/orders/{order}', 'OrderController@show')->name('orders.show');
+            });
+
+            Route::resource('categories', 'CategoryController');
+            Route::resource('products', 'ProductController');
+        });
+    });
+
+
+    Route::get('/', 'MainController@index')->name('index');
+    Route::get('/categories', 'MainController@categories')->name('categories');
+
+    Route::group(['prefix' => 'basket'], function (){
+        Route::post('/add/{id}', 'BasketController@basketAdd')->name('basket-add');
+
+        Route::group(['middleware' => 'basket_not_empty',], function (){
+            Route::get('/', 'BasketController@basket')->name('basket');
+            Route::get('/place', 'BasketController@basketPlace')->name('basket-place');
+            Route::post('/remove/{id}', 'BasketController@basketRemove')->name('basket-remove');
+            Route::post('/place', 'BasketController@basketConfirm')->name('basket-confirm');
+
+        });
+    });
+
+
+    Route::get('/{category}', 'MainController@category')->name('category');
+    Route::get('/{category}/{product?}', 'MainController@product')->name('product');
+
+});
+
+
